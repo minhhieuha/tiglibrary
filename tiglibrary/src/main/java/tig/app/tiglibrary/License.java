3d10591ac90c86dc4f84c0078c71578f01a91115
James <james@theinnovationguys.com>
2024-07-30 15:12:19 +0700

    1

---
+++ tiglibrary/src/main/java/tig/app/tiglibrary/License.java
@@ -0,0 +1,11 @@
+package tig.app.tiglibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class License {
+
+    public void CheckLicence(Context context){
+        Toast.makeText(context,"CheckLicence", Toast.LENGTH_LONG).show();
+    }
+}
